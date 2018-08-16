3e73ccc0019da0aae0a713dd1344d0b616d12d33
Sai Sravan Kumar Mukthinutalapati <saisravankumar.mukthinutalapati@factset.com>
2018-08-16 14:02:39 +0530

    Adding tab and authentication

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var restify          = require("restify");
 var botBuilder       = require("botbuilder");
 var teams            = require("botbuilder-teams");
+var request          = require("request");
+var jwtDecode       = require("jwt-decode");
 
 
 var server = restify.createServer();
@@ -18,81 +20,48 @@ server.post('/api/messages',connector.listen());
 var inMemoryStorage = new botBuilder.MemoryBotStorage();
 
 var bot = new botBuilder.UniversalBot(connector).set('storage', inMemoryStorage);
+var tenant;
 
 bot.dialog('/', function(session){
     console.log(teams.TeamsMessage.getTenantId(session.message));
-    session.send("Your bot is running. You said: %s", session.message.text);
-})
+    tenant = teams.TeamsMessage.getTenantId(session.message);
+    request.get("https://login.microsoftonline.com/{tenant}/oauth2/authorize?client_id=" + process.env.APP_CLIENT_ID +
+                "&response_type=code&redirect_uri=" + encodeURI("https://test-teams-bot.herokuapp.com/verified") +
+                "&response_mode=query&resource=" + encodeURI("https://factset.onmicrosoft.com/9fac8285-6f7b-4cab-b385-6ed8aec01fde" )+ "&state=verified", function(error, response, body){
+                    console.log(body);
+                });
+    //session.send("Your bot is running. You said: %s", session.message.text);
+});
 
+server.get("/verified", function(req, response){
+    if(req.params.error) console.log("Access Denied" + req.params.error);
+    if(req.params.state !== "verified") {console.log("CSRF error");}
+    else {
+        var code = req.params.code;
+        var options = {
+            host: "https://login.microsoftonline.com/",
+            path: tenant + "/oauth2/token",
+            headers: {
+                "Content-type":  "application/x-www-form-urlencoded",
+                "grant_type"  :  "authorization_code",
+                "client_id"   :  process.env.APP_CLIENT_ID,
+                "code"        :  code,
+                "redirect_uri":  encodeURI("https://test-teams-bot.herokuapp.com/verified"),
+                "resource"    :  encodeURI("https://factset.onmicrosoft.com/9fac8285-6f7b-4cab-b385-6ed8aec01fde"),
+            },
+        }
+        request.post(options, function(error, response, body){
+            console.log(body);
+            var responseToken = JSON.parse(body);
+            var decoded = jwt_decode(responseToken["id_token"]);
+            console.log(decoded["unique_name"]);
+            bot.send("Your bot is running. Your name is %s", decoded["unique_name"]);
+        })
+    }
+});
 
 server.get("/", function(req, response){ response.send(200,"Your app is up and running.")})
 
 server.get("/terms", function(req, response){ response.send(200, "Sample terms of Usage page")})
 
-server.get("/privacy", function(req, response){ response.send(200, "Privacy policy")})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+server.get("/privacy", function(req, response){ response.send(200, "Privacy policy")});
