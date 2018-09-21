e41984349544be5072b4db0af2a4673ffd4440f6
Sai Sravan Kumar Mukthinutalapati <saisravankumar.mukthinutalapati@factset.com>
2018-09-22 01:25:01 +0530

    +log

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ server.post('/api/messages',function(req, res, next){
                         if(key.kid !== jwtHeader.kid) return;
                         console.log("valid Key: " + jwtHeader.kid);
                         console.log("listed Key: " + key.kid);
+                        console.log("alg:" + getJson.id_token_signing_alg_values_supported);
                         jsonwebtoken.verify(bearerToken,{key : activityJson.text}, {"algorithm" : getJson.id_token_signing_alg_values_supported, "expiresIn" : jwtPayload.exp} , function(err, decoded){
                             if(err) console.log(err);
                             console.log(decoded);
