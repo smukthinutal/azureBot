a0a1b436db4f731b8d7604fc66354acbf39a60f8
Sai Sravan Kumar Mukthinutalapati <saisravankumar.mukthinutalapati@factset.com>
2018-09-24 11:55:04 +0530

    +test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var request          = require("request");
 var jwtDecoder       = require("jwt-decode");
 var rn               = require('random-number');
 var jsonwebtoken     = require("jsonwebtoken");
+var jwkToPem         = require("jwk-to-pem");
 
 
 var server = restify.createServer();
@@ -64,7 +65,7 @@ server.post('/api/messages',function(req, res, next){
                         console.log("valid Key: " + jwtHeader.kid);
                         console.log("listed Key: " + key.kid);
                         console.log("alg:" + getJson.id_token_signing_alg_values_supported[0] + typeof getJson.id_token_signing_alg_values_supported[0]);
-                        jsonwebtoken.verify(bearerToken, key.toString(), {"algorithm" : "RS256", "expiresIn" : jwtPayload.exp} , function(err, decoded){
+                        jsonwebtoken.verify(bearerToken, jwkToPem(key.toString()), {"algorithm" : "RS256", "expiresIn" : jwtPayload.exp} , function(err, decoded){
                             if(err) console.log(err);
                             console.log(decoded);
                         })
